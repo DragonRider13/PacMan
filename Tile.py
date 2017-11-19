7cadbc9a16de3c9c2c6cef1e68e8aca6776ede65
DragonRider13 <31828118+DragonRider13@users.noreply.github.com>
2017-11-19 12:16:19 -0800

    Create Tile.py

---
+++ Tile.py
@@ -0,0 +1,9 @@
+class Tile(object):
+    def __init__(self, dim, pos=[0,0]):
+        self.dim = dim
+        self.pos = pos
+        self.COLOR = (255,0,0)
+
+    def draw(self, screen):
+        values = list(self.pos)+list(self.dim)
+        pygame.draw.rect(screen, self.COLOR, values)
